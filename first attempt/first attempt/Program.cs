901b6145e6ac70e1eb0a01ce1ae2f9b40528cc46
Maeve <you@example.com>
2022-05-26 16:31:13 +1200

    first attempt auto test login

---
+++ first attempt/first attempt/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+//open chrom browser
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+//launch turn up portal
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+//find username textbox and input username
+IWebElement UsernameTextbox = driver.FindElement(By.Id("UserName"));
+UsernameTextbox.SendKeys("hari");
+
+//find password textbox and input password
+IWebElement PasswordTextbox = driver.FindElement(By.Id("Password"));
+PasswordTextbox.SendKeys("123123");
+//find login button and click
+IWebElement LoginButton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+LoginButton.Click();
+
+//check whether login successfully
+IWebElement hellohari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
+
+if (hellohari.Text == "Hello hari!")
+
+    Console.WriteLine("login successfully, test passed");
+
+else
+    Console.WriteLine("login failed, test failed");

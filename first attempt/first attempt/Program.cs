6c08ed96f047c066e0048d4b1e4856ce045fbe41
Maeve <you@example.com>
2022-05-28 03:32:29 +1200

    Auto test edit function added

--- first attempt/first attempt/Program.cs
+++ first attempt/first attempt/Program.cs
@@ -15,6 +15,7 @@ UsernameTextbox.SendKeys("hari");
 //find password textbox and input password
 IWebElement PasswordTextbox = driver.FindElement(By.Id("Password"));
 PasswordTextbox.SendKeys("123123");
+
 //find login button and click
 IWebElement LoginButton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
 LoginButton.Click();
@@ -29,6 +30,7 @@ if (hellohari.Text == "Hello hari!")
 
 else
     Console.WriteLine("login failed, test failed");
+
 //identify administration and click
 IWebElement Administration = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
 Administration.Click();
@@ -59,13 +61,14 @@ DescriptionTextbox.SendKeys("654352");
 
 //identify unit price textbox and input price
 IWebElement inputPriceTextBox = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span/input[1]"));
-inputPriceTextBox.SendKeys("20");
+inputPriceTextBox.Click();
+IWebElement priceTextbox = driver.FindElement(By.Id("Price"));
+priceTextbox.SendKeys("20");
 
 //identify save button and click
 IWebElement SaveButton = driver.FindElement(By.XPath("//*[@id='SaveButton']"));
 SaveButton.Click();
 Thread.Sleep(1500);
-
 
 //check whether create successfully
 IWebElement LastPageButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
@@ -81,3 +84,61 @@ else
 {
     Console.WriteLine("Create failed, test failed");
 }
+Thread.Sleep(1000);
+
+//Identify edit button and click
+IWebElement editButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[5]/a[1]"));
+editButton.Click();
+
+//iddentify typecode dropdownbox and change option
+IWebElement EditTMDropdownBox = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[1]/div/span[1]/span/span[1]"));
+EditTMDropdownBox.Click();
+Thread.Sleep(1000);
+
+IWebElement materialOption = driver.FindElement(By.XPath("//*[@id='TypeCode_listbox']/li[1]"));
+materialOption.Click();
+
+//identify code textbox and change code name
+IWebElement EditCodeTextbox = driver.FindElement(By.Id("Code"));
+EditCodeTextbox.Clear();
+EditCodeTextbox.SendKeys("TEFA123");
+
+//identify description textbox and change description
+IWebElement editDescriptionTextbox = driver.FindElement(By.Id("Description"));
+editDescriptionTextbox.Clear();
+editDescriptionTextbox.SendKeys("C123123");
+
+//identify price textbox and change price
+IWebElement EditinputPriceTextBox = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span/input[1]"));
+EditinputPriceTextBox.Click();
+
+IWebElement EditpriceTextbox = driver.FindElement(By.Id("Price"));
+EditpriceTextbox.Clear();
+//Thread.Sleep(1000);
+
+EditinputPriceTextBox.Click();
+EditpriceTextbox.SendKeys("100");
+
+//identify save button and click
+IWebElement EditSaveButton = driver.FindElement(By.XPath("//*[@id='SaveButton']"));
+EditSaveButton.Click();
+Thread.Sleep(1500);
+
+//check whether edit successfully
+
+IWebElement AgainLastPageButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
+AgainLastPageButton.Click();
+
+IWebElement CodeShowed = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
+IWebElement typeShowed = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[2]"));
+IWebElement DescShowed = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[3]"));
+IWebElement priceShowed = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[4]"));
+if (CodeShowed.Text == "TEFA123"
+    && typeShowed.Text == "M"
+    && DescShowed.Text == "C123123"
+    && priceShowed.Text == "$100.00")
+{
+    Console.WriteLine("Edit successfully, test pass");
+}
+else
+    Console.WriteLine("Edit Failed, test failed");

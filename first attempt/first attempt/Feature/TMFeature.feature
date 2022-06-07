cc91052bbbd2d6da9d2e83a0fe7dd42f7ed49353
Maeve <you@example.com>
2022-06-08 01:39:01 +1200

    specflow added

---
+++ first attempt/first attempt/Feature/TMFeature.feature
@@ -0,0 +1,12 @@
+﻿Feature: TMFeature
+
+As a TurnUp protal admin
+I would like to create, edit and delete time and material records
+So that I can manage employee' time and materials successfully
+
+
+Scenario: create time&material records with valid details
+	Given I login to turnup portal successfully
+	When I navigate to Time&Material page
+	And I create a new time/material record
+	Then A new T&M record should be create successfully
---
+++ first attempt/first attempt/StepDefinotions/TMFeatureStepDefinition.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace first_attempt.StepDefinotions
+{
+    [Binding]
+    public class TMFeatureStepDefinition
+    {
+        [Given(@"I login to turnup portal successfully")]
+        public void GivenILoginToTurnupPortalSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I navigate to Time&Material page")]
+        public void WhenINavigateToTimeMaterialPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I create a new time/material record")]
+        public void WhenICreateANewTimeMaterialRecord()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"A new T&M record should be create successfully")]
+        public void ThenANewTMRecordShouldBeCreateSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+
+
+
+
+    }
+}
